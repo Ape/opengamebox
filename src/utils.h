0d0b4f8836d4703d00d112e5a3b7c08c5f6f5909
Lauri Niskanen <ape@ape3000.com>
2012-03-04 10:57:23 +0200

    Licensed as GPLv3

--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,20 @@
+// Copyright 2012 Lauri Niskanen
+//
+// This file is part of OpenGamebox.
+//
+// OpenGamebox is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenGamebox is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenGamebox.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef UTILS_H
 #define UTILS_H
 
