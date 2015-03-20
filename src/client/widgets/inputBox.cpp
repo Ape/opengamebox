Ape/opengamebox

commit 1a9a30e87e4cbc70ba37cf1b6cd85b4a497859ab

Inputbox now wraps text

src/client/widgets/inputBox.cpp
@@ -18,26 +18,30 @@
 
 #include "inputBox.h"
 
-InputBox::InputBox(Game *game, void (Game::*send)(std::string), std::string caption, Vector2 location, float width, unsigned char maxLen)
-: Widget(location, Vector2(width, 20.0f)) {
-	this->game = game;
-	this->send = send;
-	this->caption = caption;
-	this->text = al_ustr_new("");
-	this->inputLocation = 0;
-	this->maxLength = maxLen;
-	this->historyIndex = 0;
-}
+InputBox::InputBox(Game *game, void (Game::*send)(std::string), std::string caption, Vector2 location, float width, unsigned char maxLen, ALLEGRO_FONT *font)
+: Widget(location, Vector2(width, 20.0f)),
+	game(game),
+	send(send),
+	textWidget(new TextArea(location, size, caption + ": |", font)),
+	caption(caption),
+	text(al_ustr_new("")),
+	inputLocation(0),
+	maxLength(maxLen),
+	historyIndex(0) {}
 
 InputBox::~InputBox() {
 	al_ustr_free(this->text);
+	delete this->textWidget;
 }
 
 void InputBox::draw(IRenderer *renderer) {
-	renderer->drawRectangleFilled(this->location, this->location + this->size, Color(0.15f, 0.15f, 0.15f));
-	renderer->drawRectangle(this->location, this->location + this->size, Color(1.0f, 1.0f, 1.0f), 1.0f);
 
-	std::ostringstream tmpText;
+	renderer->drawRectangleFilled(this->location, this->location + Vector2(this->size.x, this->textWidget->getLineCount() * 20), Color(0.15f, 0.15f, 0.15f));
+	renderer->drawRectangle(this->location, this->location + Vector2(this->size.x, this->textWidget->getLineCount() * 20), Color(1.0f, 1.0f, 1.0f), 1.0f);
+
+	textWidget->draw(renderer);
+
+/*	std::ostringstream tmpText;
 
 	tmpText.str(std::string());
 
@@ -55,6 +59,8 @@ void InputBox::draw(IRenderer *renderer) {
 	al_ustr_free(tmpUstr);
 
 	renderer->drawText(tmpText.str(), Vector2(this->location));
+
+*/
 }
 
 bool InputBox::onKey(ALLEGRO_KEYBOARD_EVENT keyboard) {
@@ -106,6 +112,27 @@ bool InputBox::onKey(ALLEGRO_KEYBOARD_EVENT keyboard) {
 		++this->inputLocation;
 	}
 
+	if (keyboard.keycode != ALLEGRO_KEY_ENTER && keyboard.keycode != ALLEGRO_KEY_DELETE) {
+
+		std::ostringstream tmpText;
+
+		tmpText.str(std::string());
+
+		ALLEGRO_USTR *tmpUstr = al_ustr_dup_substr(this->text, 0, al_ustr_offset(this->text, this->inputLocation));
+
+		if (! this->caption.empty()) {
+			tmpText << this->caption << ": ";
+		}
+
+		tmpText << al_cstr(tmpUstr) << "|";
+		al_ustr_free(tmpUstr);
+
+		tmpUstr = al_ustr_dup_substr(this->text, al_ustr_offset(this->text, this->inputLocation), al_ustr_size(this->text));
+		tmpText << al_cstr(tmpUstr);
+		al_ustr_free(tmpUstr);
+
+		textWidget->setText(tmpText.str());
+	}
 	return true;
 }
 

src/client/main.cpp
@@ -446,7 +446,7 @@ void Game::localEvents() {
 		} else if (event.keyboard.keycode == ALLEGRO_KEY_ENTER) {
 			// Create a new chat input widget
 			this->input = new InputBox(this, &Game::sendChat, "Chat", Vector2(2.0f, this->renderer->getDisplaySize().y / 2.0f + 20),
-										300.0f, 255);
+										300.0f, 255, this->renderer->getFont());
 		}
 	} else if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
 		if (input == nullptr) {
@@ -1633,7 +1633,7 @@ void Game::checkObjectOrder() {
 
 void Game::askNick() {
 	this->input = new InputBox(this, &Game::identifyToServer, "Nick", Vector2(2.0f, this->renderer->getDisplaySize().y / 2.0f + 20.0f),
-								225.0f, 16);
+								225.0f, 16, this->renderer->getFont());
 }
 
 void Game::identifyToServer(std::string nick) {

src/client/widgets/inputBox.h
@@ -22,12 +22,13 @@
 #include <sstream>
 
 #include "../widget.h"
+#include "textarea.h"
 
 class Game;
 
 class InputBox : public Widget {
 public:
-	InputBox(Game *game, void (Game::*send)(std::string), std::string caption, Vector2 location, float width, unsigned char maxLen);
+	InputBox(Game *game, void (Game::*send)(std::string), std::string caption, Vector2 location, float width, unsigned char maxLen, ALLEGRO_FONT *font);
 	virtual ~InputBox();
 
 	virtual void draw(IRenderer *renderer);
@@ -41,6 +42,7 @@ private:
 	Game *game;
 	void (Game::*send)(std::string);
 
+	TextArea *textWidget;
 	std::string caption;
 	ALLEGRO_USTR *text;
 	size_t inputLocation;

src/client/widgets/textarea.cpp
@@ -20,22 +20,26 @@
 TextArea::TextArea(Vector2 location, Vector2 size, std::string text, ALLEGRO_FONT *font)
 		  : Widget(location, size),
 		    font(font) {
+
 	this->text = utils::splitString(text, ' ');
-	{
-		int lines = 1;
-		int width = 0;
-		for (auto &word : this->text) {
-			int textwidth = al_get_text_width(this->font, word.c_str());
-			if (width + textwidth > this->size.x) {
-				lines++;
-				width = 0;
-			} else {
-				width += al_get_text_width(this->font, " ");
-			}
-			width += textwidth;
+	this->countLines();
+}
+
+void TextArea::countLines()
+{
+	int lines = 1;
+	int width = 0;
+	for (auto &word : this->text) {
+		int textwidth = al_get_text_width(this->font, word.c_str());
+		if (width + textwidth > this->size.x) {
+			lines++;
+			width = 0;
+		} else {
+			width += al_get_text_width(this->font, " ");
 		}
-		this->lineCount = lines;
+		width += textwidth;
 	}
+	this->lineCount = lines;
 }
 
 TextArea::~TextArea() {
@@ -70,6 +74,11 @@ void TextArea::draw(IRenderer *renderer) {
 
 int TextArea::getLineCount() {
 	return this->lineCount;
+}
+
+void TextArea::setText(std::string text) {
+	this->text = utils::splitString(text, ' ');
+	this->countLines();
 }
 
 void TextArea::move(Vector2 location) {

src/client/widgets/textarea.h
@@ -31,10 +31,14 @@ public:
 
 	int getLineCount();
 
+	void setText(std::string text);
+
 	void move(Vector2 location);
 	Vector2 getLocation();
 
 private:
+	void countLines();
+
 	std::vector<std::string> text;
 	ALLEGRO_FONT *font;
 	int lineCount;

commit 738bd6ba2d238a4979eafc1178bfaf61e9a15e16

Fix InputBox crashes
Commit 1a9a30e8 (text wrapping) caused crashes on esc key press and delete key
working weirdly, but this seems to fix the issues.

src/client/widgets/inputBox.cpp
@@ -40,27 +40,6 @@ void InputBox::draw(IRenderer *renderer) {
 	renderer->drawRectangle(this->location, this->location + Vector2(this->size.x, this->textWidget->getLineCount() * 20), Color(1.0f, 1.0f, 1.0f), 1.0f);
 
 	textWidget->draw(renderer);
-
-/*	std::ostringstream tmpText;
-
-	tmpText.str(std::string());
-
-	ALLEGRO_USTR *tmpUstr = al_ustr_dup_substr(this->text, 0, al_ustr_offset(this->text, this->inputLocation));
-
-	if (! this->caption.empty()) {
-		tmpText << this->caption << ": ";
-	}
-
-	tmpText << al_cstr(tmpUstr) << "|";
-	al_ustr_free(tmpUstr);
-
-	tmpUstr = al_ustr_dup_substr(this->text, al_ustr_offset(this->text, this->inputLocation), al_ustr_size(this->text));
-	tmpText << al_cstr(tmpUstr);
-	al_ustr_free(tmpUstr);
-
-	renderer->drawText(tmpText.str(), Vector2(this->location));
-
-*/
 }
 
 bool InputBox::onKey(ALLEGRO_KEYBOARD_EVENT keyboard) {
@@ -112,8 +91,7 @@ bool InputBox::onKey(ALLEGRO_KEYBOARD_EVENT keyboard) {
 		++this->inputLocation;
 	}
 
-	if (keyboard.keycode != ALLEGRO_KEY_ENTER && keyboard.keycode != ALLEGRO_KEY_DELETE) {
-
+	if (keyboard.keycode != ALLEGRO_KEY_ENTER && keyboard.keycode != ALLEGRO_KEY_ESCAPE) {
 		std::ostringstream tmpText;
 
 		tmpText.str(std::string());
@@ -133,6 +111,7 @@ bool InputBox::onKey(ALLEGRO_KEYBOARD_EVENT keyboard) {
 
 		textWidget->setText(tmpText.str());
 	}
+
 	return true;
 }
 